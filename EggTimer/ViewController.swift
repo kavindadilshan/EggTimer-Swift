dfc4162c3b8bd73d5dc7b9b1dbb8ef9210d76572
kavindadilshan <kavindadilshan97@gmail.com>
2022-08-16 10:30:27 +0530

    ranger selector and optionals

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,8 +8,40 @@
 
 import UIKit
 
+/**
+ Range Oparators
+        a...b               // Closed Operat
+        a..<b     // half open range
+        ..b               // One Side Range
+ */
+
+/**
+ Optionals
+   var player: String ?= nil
+   player = "kavinda"
+   print(player) //optional(kavinda)
+   print(player!) // kavinda
+   player = nil
+   print(player) //exception
+ **/
+
 class ViewController: UIViewController {
     
-
+    let softTime = 2
+    let mediumTime = 5
+    let hardTime = 10
+    
+    @IBAction func eggBtnPress(_ sender: UIButton) {
+        var hardness = sender.currentTitle
+        
+        if hardness == "Soft" {
+            print(softTime)
+        }else if hardness == "Medium"{
+            print(mediumTime)
+        }else{
+            print(hardTime)
+        }
+    }
+    
 
 }
